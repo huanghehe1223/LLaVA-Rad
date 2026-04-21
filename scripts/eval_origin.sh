ebb216a3574bae01b3631a82be7ab8750bd78820
huanghehe1223 <h3145267275@126.com>
2026-04-21 15:14:04 +0000

    更新 eval.sh 和 eval_modal.sh 脚本，调整模型路径和预测目录，合并预测文件输出，新增 eval_origin.sh 脚本以支持原始评估流程

--- scripts/eval_origin.sh
+++ scripts/eval_origin.sh
@@ -3,27 +3,24 @@
 set -e
 set -o pipefail
 
-model_base=/cxr-report/model/vicuna-7b-v1.5
+model_base=lmsys/vicuna-7b-v1.5
 model_path=microsoft/llava-rad
 
 model_base="${1:-$model_base}"
 model_path="${2:-$model_path}"
 prediction_dir="${3:-/cxr-report/llavarad}"
 prediction_file=$prediction_dir/test
-merged_prediction_file=$prediction_dir/mimic_cxr_preds.jsonl
 
 run_name="${4:-llavarad}"
 
 
-query_file=/cxr-report/json_files/test.json
+# query_file=/PATH_TO/physionet.org/files/llava-rad-mimic-cxr-annotation/1.0.0/chat_test_MIMIC_CXR_all_gpt4extract_rulebased_v1.json
 
-image_folder=/cxr-report/dataset
+# image_folder=/PATH_TO/physionet.org/files/mimic-cxr-jpg/2.0.0/files
 loader="mimic_test_findings"
 conv_mode="v1"
 
 CHUNKS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
-
-mkdir -p "${prediction_dir}"
 
 for (( idx=0; idx<$CHUNKS; idx++ ))
 do
@@ -44,9 +41,11 @@ done
 
 wait
 
-cat ${prediction_file}_*.jsonl > "${merged_prediction_file}"
+cat ${prediction_file}_*.jsonl > mimic_cxr_preds.jsonl
 
 pushd llava/eval/rrg_eval
 WANDB_PROJECT="llava" WANDB_RUN_ID="llava-eval-$(date +%Y%m%d%H%M%S)" WANDB_RUN_GROUP=evaluate CUDA_VISIBLE_DEVICES=0 \
-    python run.py "${merged_prediction_file}" --run_name ${run_name} --output_dir "${prediction_dir}/eval"
+    python run.py ../../../mimic_cxr_preds.jsonl --run_name ${run_name} --output_dir ../../../${prediction_dir}/eval
 popd
+
+rm mimic_cxr_preds.jsonl
